99b52ce98a864ea0305db2060d565e19dc89e7d6
jamesrosstwo <james.rosstwo@gmail.com>
2019-03-17 01:33:05 -0400

    add titles and usernames

--- HackRPI/Source.cpp
+++ HackRPI/Source.cpp
@@ -11,6 +11,8 @@
 #include <vector>
 #include "PasswordGenerator.h"
 
+#define IDC_MAIN_EDIT	101
+
 // The main window class name.
 static TCHAR szWindowClass[] = _T("Source");
 static int frameWidth = 1400;
@@ -24,6 +26,8 @@ HFONT defaultFont;
 HWND parentWindow;
 HWND genPasswordButton;
 HWND languageSelector;
+HWND titleBox;
+HWND usernameBox;
 HWND numWordsSelector;
 HINSTANCE hInst;
 
@@ -71,6 +75,7 @@ TCHAR Languages[12][11] =
 void initColourScheme() {
 	COLORREF lightBlue = 0xffa670;
 	COLORREF midBlue = 0xf48642;
+	COLORREF darkBlue = 0xfc271b;
 	COLORREF midCharcoal = 0x3d3d3d;
 	COLORREF darkCharcoal = 0x262626;
 	COLORREF accentGold = 0x49f2ff;
@@ -78,6 +83,7 @@ void initColourScheme() {
 
 	colourScheme.insert(std::pair<LPCWSTR, COLORREF>(L"lightBlue", lightBlue));
 	colourScheme.insert(std::pair<LPCWSTR, COLORREF>(L"midBlue", midBlue));
+	colourScheme.insert(std::pair<LPCWSTR, COLORREF>(L"darkBlue", darkBlue));
 	colourScheme.insert(std::pair<LPCWSTR, COLORREF>(L"midCharcoal", midCharcoal));
 	colourScheme.insert(std::pair<LPCWSTR, COLORREF>(L"darkCharcoal", darkCharcoal));
 	colourScheme.insert(std::pair<LPCWSTR, COLORREF>(L"accentGold", accentGold));
@@ -102,12 +108,8 @@ void setFont(HWND hw, LPCWSTR fontChoice) {
 	SendMessage(hw, WM_SETFONT, (WPARAM)getFont(fontChoice), TRUE);
 }
 
-HFONT setFont(HDC hdc, long fontSize, LPCWSTR fontChoice, bool isBold, bool isItalic) {
+HFONT setFont(HDC hdc, long fontSize, LPCWSTR fontChoice, UINT weight, bool isItalic) {
 	HFONT hf;
-	UINT weight = FW_DONTCARE;
-	if (isBold) {
-		weight = FW_SEMIBOLD;
-	}
 
 	hf = CreateFont(fontSize, 0, 0, 0, weight, FALSE, isItalic, 0, 0, 0, 0, 0, 0, fontChoice);
 	SelectObject(hdc, hf);
@@ -115,20 +117,20 @@ HFONT setFont(HDC hdc, long fontSize, LPCWSTR fontChoice, bool isBold, bool isIt
 }
 
 HWND createLanguageSelector() {
-	int xpos = 100;            // Horizontal position of the window.
+	int xpos = 300;            // Horizontal position of the window.
 	int ypos = 100;            // Vertical position of the window.
-	int nwidth = 200;          // Width of the window
-	int nheight = 200;         // Height of the window
+	int nwidth = 250;          // Width of the window
+	int nheight = 300;         // Height of the window
 
 	HWND hWndComboBox = CreateWindow(WC_COMBOBOX, TEXT(""),
-		CBS_DROPDOWN | CBS_HASSTRINGS | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE,
+		CBS_DROPDOWNLIST | CBS_HASSTRINGS | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE,
 		xpos, ypos, nwidth, nheight, parentWindow, NULL, hInst,
 		NULL);
 	setFont(hWndComboBox, defaultFontType);
-	TCHAR A[16];
+	TCHAR A[24];
 	int i = 0;
 	memset(&A, 0, sizeof(A));
-	for (i = 0; i <= 12; i += 1)
+	for (i = 0; i <= 10; i += 1)
 	{
 		wcscpy_s(A, sizeof(A) / sizeof(TCHAR), (TCHAR*)Languages[i]);
 		// Add string to combobox.
@@ -136,6 +138,40 @@ HWND createLanguageSelector() {
 	}
 	SendMessage(hWndComboBox, CB_SETCURSEL, (WPARAM)0, (LPARAM)0);
 	return hWndComboBox;
+}
+
+HWND createTitleBox() {
+	int x = 300;
+	int y = 165;
+	int w = 250;
+	int h = 30;
+
+	HWND hw = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+		WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
+		x, y, w, h, parentWindow, (HMENU)IDC_MAIN_EDIT, GetModuleHandle(NULL), NULL);
+	setFont(hw, defaultFontType);
+	return hw;
+}
+
+HWND createUsernameBox() {
+	int x = 300;
+	int y = 230;
+	int w = 250;
+	int h = 30;
+
+	HWND hw = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"",
+		WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
+		x, y, w, h, parentWindow, (HMENU)IDC_MAIN_EDIT, GetModuleHandle(NULL), NULL);
+	setFont(hw, defaultFontType);
+	return hw;
+}
+
+std::wstring getTextFromTextBox(HWND tBox) {
+    int length = GetWindowTextLengthW(tBox);
+    std::wstring buf (length+1, '\000'); // + 1 for extra null GetWindowTextW wants to write
+    GetWindowTextW(tBox, buf.data(), buf.size());
+    buf.resize(length); // throw away extra null, std::(w)string already has one
+    return buf;
 }
 
 int CALLBACK WinMain(
@@ -200,11 +236,14 @@ int CALLBACK WinMain(
 
 	languageSelector = createLanguageSelector();
 
+	titleBox = createTitleBox();
+	usernameBox = createUsernameBox();
+
 	genPasswordButton = CreateWindow(
 		L"BUTTON",  // Predefined class; Unicode assumed 
 		L"Generate",      // Button text 
 		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
-		500,         // x position 
+		150,         // x position 
 		650,         // y position 
 		400,        // Button width
 		50,        // Button height
@@ -262,19 +301,35 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 	{
 		hdc = BeginPaint(hWnd, &ps);
-		setFont(hdc, 30, defaultFontType, true, false);
-		SetBkColor(hdc, colourScheme.at(L"bg"));
+		setFont(hdc, 45, defaultFontType, FW_DONTCARE, false);
+		SetBkMode(hdc, TRANSPARENT);
 		SetTextColor(hdc, colourScheme.at(L"white"));
 		TextOut(hdc,
 			534, 20,
 			frameTitle, _tcslen(frameTitle));
+
+
+		SetTextColor(hdc, colourScheme.at(L"lightBlue"));
+		setFont(hdc, 28, defaultFontType, FW_THIN, false);
+
+		TextOut(hdc,
+			150, 100,
+			L"Language", _tcslen(L"Language"));
+
+		TextOut(hdc,
+			150, 164,
+			L"Title", _tcslen(L"Title"));
+
+		TextOut(hdc,
+			150, 230,
+			L"Username", _tcslen(L"Username"));
 
 		int iter = 0;
 		for (Account acc : accounts) {
 			OutputDebugString(L"Working");
 			std::wstring txt = acc.title + _T(" - ") + acc.username.c_str() + L": " + acc.password.c_str();
 			TextOut(hdc,
-				200, 400 + iter * 40,
+				650, 94 + iter * 40,
 				txt.c_str(), _tcslen(txt.c_str()));
 			iter++;
 		}
@@ -286,8 +341,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		if ((HWND)lParam == genPasswordButton) {
 			Account acc;
-			acc.title = _T("testTitle");
-			acc.username = _T("testUsername");
+			acc.title = getTextFromTextBox(titleBox);
+			acc.username = getTextFromTextBox(usernameBox);
 			acc.password = genPass(30, currentLang);;
 			accounts.push_back(acc);
 
