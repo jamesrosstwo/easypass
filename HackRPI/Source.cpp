07a624f59918186d8b74fcc12a8dda57ecd0e6b2
jamesrosstwo <james.rosstwo@gmail.com>
2019-03-16 20:46:52 -0400

    add String to widestring

--- HackRPI/Source.cpp
+++ HackRPI/Source.cpp
@@ -40,6 +40,15 @@ std::string WidestringToString(const std::wstring &wstr)
 	return strTo;
 }
 
+std::wstring StringToWidestring(const std::string &str)
+{
+	if (str.empty()) return std::wstring();
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+	std::wstring wstrTo(size_needed, 0);
+	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+	return wstrTo;
+}
+
 TCHAR Languages[12][11] =
 {
 	TEXT("English"), TEXT("French"), TEXT("Italian"), TEXT("Spanish"),
